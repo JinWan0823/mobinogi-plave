61a493de73bc6ded746dd9e46d2d4e4e5108f735
jinWan0823 <dhksl823@gmail.com>
2025-07-17 20:51:19 +0900

    💄 메인페이지 리스트 레이아웃 제작

--- src/_components/mainpage/MainBanner.tsx
+++ src/_components/mainpage/MainBanner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MainBanner() {
   return (
     <div className="w-full min-h-[100vh] bg-main relative overflow-hidden">
-      <div className="inner text-center pt-70">
+      <div className="inner text-center pt-70 z-999999">
         <h1 className="text-8xl text-white">
           모비노기 데이안 서버 <br />
           플레이브 길드
@@ -17,19 +17,21 @@ export default function MainBanner() {
         <button
           className={`title bg-white text-point
           text-2xl py-3 px-16
-          rounded-full border-2 border-point`}
+          rounded-full border-2 border-point
+          `}
         >
           Main Btn
         </button>
       </div>
 
+      {/* 이미지 출처:  https://cafe.daum.net/plave/aaH8/6856?svc=TOPRANK (플레이브 팬카페) */}
       <Image
         draggable="false"
         src={"/main/plave_banner.png"}
         width={1640}
         height={575}
         alt="플레이브 로고 이미지"
-        className="absolute bottom-[-120px] left-1/2 -translate-x-1/2"
+        className="absolute bottom-[-120px] left-1/2 -translate-x-1/2 z-[0]"
       />
     </div>
   );
