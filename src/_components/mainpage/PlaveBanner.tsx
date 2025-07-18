16fac2dfa258565e6aa5f3dc1bb6bb1489aa40ab
jinWan0823 <dhksl823@gmail.com>
2025-07-18 16:50:22 +0900

    🎨 가로스크롤 커스텀훅 제작

--- src/_components/mainpage/PlaveBanner.tsx
+++ src/_components/mainpage/PlaveBanner.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useHorizontalScroll } from "@/_hooks/useHorizontalScroll";
 import Image from "next/image";
 
 export default function PlaveBanner() {
+  const listWrapperRef = useHorizontalScroll();
   return (
     <section
       className="bg-cover bg-center relative py-28"
+      //이미지 출처 - https://namu.wiki/w/%E3%81%8B%E3%81%8F%E3%82%8C%E3%82%93%E3%81%BC(PLAVE)
       style={{ backgroundImage: "url(/main/plave_banner2.png)" }}
     >
       <div
@@ -11,8 +16,9 @@ export default function PlaveBanner() {
         absolute inset-0 bg-[#000000de]"
       />
 
-      <div className="inner relative z-999">
+      <div className=" relative z-999">
         <div className="text-center flex flex-col justify-center items-center">
+          {/* 이미지 출처 - https://namu.wiki/w/%E3%81%8B%E3%81%8F%E3%82%8C%E3%82%93%E3%81%BC(PLAVE) */}
           <Image
             src={"/main/plave_logo.png"}
             width={240}
@@ -23,11 +29,18 @@ export default function PlaveBanner() {
             플레이브의 최신 유튜브 동영상입니다
           </p>
         </div>
-        <div className="flex mt-8 gap-4">
-          <li className="w-[464px] h-[260px] bg-[#aaaaaa] rounded-[20px]"></li>
-          <li className="w-[464px] h-[260px] bg-[#aaaaaa] rounded-[20px]"></li>
-          <li className="w-[464px] h-[260px] bg-[#aaaaaa] rounded-[20px]"></li>
-        </div>
+
+        <ul
+          className="scrollbar-none overflow-x-auto gap-4 flex mt-6 px-10"
+          ref={listWrapperRef}
+        >
+          {[1, 2, 3, 4, 5].map((_, i) => (
+            <li
+              key={i}
+              className="w-[464px] h-[260px] bg-[#aaaaaa] rounded-[20px] flex-shrink-0 snap-start"
+            ></li>
+          ))}
+        </ul>
       </div>
     </section>
   );
