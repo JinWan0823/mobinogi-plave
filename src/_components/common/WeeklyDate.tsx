30deeb94c9604aa225ce5482f5c34bc6b97d5408
jinWan0823 <dhksl823@gmail.com>
2025-07-23 23:06:49 +0900

    ✨ 24시간 동안 플래너 숨기기 기능 추가

--- src/_components/common/WeeklyDate.tsx
+++ src/_components/common/WeeklyDate.tsx
@@ -1,5 +1,13 @@
 export default function WeeklyDate() {
+  const today = new Date();
   const month = new Date().getMonth() + 1;
 
-  return <p className="text-sm text-center font-bold">{month}월 3주차</p>;
+  const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+  const weekNumber = Math.ceil((today.getDate() + firstDay.getDay()) / 7);
+
+  return (
+    <p className="text-sm text-center font-bold">
+      {month}월 {weekNumber}주차
+    </p>
+  );
 }
