36f22373e65dc351354c0af74a393aff0a782046
jinWan0823 <dhksl823@gmail.com>
2025-08-07 19:08:06 +0900

    🔧 정적 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const press = Press_Start_2P({
   subsets: ["latin"],
   variable: "--font-Press",
 });
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
 export const metadata = {
   title: {
@@ -24,12 +25,29 @@ export const metadata = {
     template: "%s | MOBINOGI - PLAVE",
   },
   description: "한눈에 보는 모비노기와 플레이브 소식",
+  keywords: ["모비노기", "플레이브", "팬길드", "공략", "이벤트", "자유게시판"],
+  openGraph: {
+    title: "MOBINOGI - PLAVE",
+    description:
+      "한눈에 보는 모비노기와 플레이브 소식! 모비노기 플레이브 팬 길드 홈페이지",
+    images: [`${baseUrl}/og/home.png`],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MOBINOGI - PLAVE",
+    description:
+      "한눈에 보는 모비노기와 플레이브 소식! 모비노기 플레이브 팬 길드 홈페이지",
+    images: [`${baseUrl}/og/home.png`],
+  },
   manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-icon-180x180.png",
     shortcut: "/icon-192x192.png",
   },
+  applicationName: "MOBINOGI - PLAVE",
+  category: "community",
+  author: "모비노기 플레이브 팬 길드",
 };
 export default function RootLayout({
   children,
